4a661a1adfd5f194dace77866aff0aab08d47c1e
Efrain May <efrain_may_75@hotmail.com>
2021-01-09 11:11:09 -0600

    HorizontalPage y se resolvio enl problema de los card usando el widgte expanded

--- lib/src/models/pelicula_model.dart
+++ lib/src/models/pelicula_model.dart
@@ -1,20 +1,20 @@
-class Peliculas{
+class Peliculas {
+
   List<Pelicula> items = new List();
 
   Peliculas();
 
-  Peliculas.fromJsonList(List<dynamic> jsonList ){
-    
-    if(jsonList == null) return;
+  Peliculas.fromJsonList( List<dynamic> jsonList  ) {
 
-    for (var item in jsonList) {
+    if ( jsonList == null ) return;
+
+    for ( var item in jsonList  ) {
       final pelicula = new Pelicula.fromJsonMap(item);
-      items.add(pelicula);
+      items.add( pelicula );
     }
+
   }
-
 }
-
 class Pelicula {
   int voteCount;
   int id;
@@ -23,7 +23,7 @@ class Pelicula {
   String title;
   double popularity;
   String posterPath;
-  OriginalLanguage originalLanguage;
+  String originalLanguage;
   String originalTitle;
   List<int> genreIds;
   String backdropPath;
@@ -32,38 +32,58 @@ class Pelicula {
   String releaseDate;
 
   Pelicula({
-    this.adult,
-    this.backdropPath,
-    this.genreIds,
+    this.voteCount,
     this.id,
-    this.originalLanguage,
-    this.originalTitle,
-    this.overview,
-    this.popularity,
-    this.posterPath,
-    this.releaseDate,
-    this.title,
     this.video,
     this.voteAverage,
-    this.voteCount,
+    this.title,
+    this.popularity,
+    this.posterPath,
+    this.originalLanguage,
+    this.originalTitle,
+    this.genreIds,
+    this.backdropPath,
+    this.adult,
+    this.overview,
+    this.releaseDate,
   });
 
-  Pelicula.fromJsonMap( Map<String, dynamic> json){
-    adult             = json['adult'];
-    backdropPath      = json['backdrop_path'];
-    genreIds          = json['genre_ids'].cast<int>();
-    id                = json['id'];
-    originalLanguage  = json['original_language'];
-    originalTitle     = json['original_title'];
-    overview          = json['overview'];
-    popularity        = json['popularity'] / 1;
-    posterPath        = json['poster_path'];
-    releaseDate       = json['release_date'];
-    title             = json['title'];
-    video             = json['video'];
-    voteAverage       = json['vote_average'] / 1;
-    voteCount         = json['vote_count'];
-  }
-}
+  Pelicula.fromJsonMap( Map<String, dynamic> json ) {
 
-enum OriginalLanguage { EN, JA, DE, NO }
+    voteCount        = json['vote_count'];
+    id               = json['id'];
+    video            = json['video'];
+    voteAverage      = json['vote_average'] / 1;
+    title            = json['title'];
+    popularity       = json['popularity'] / 1;
+    posterPath       = json['poster_path'];
+    originalLanguage = json['original_language'];
+    originalTitle    = json['original_title'];
+    genreIds         = json['genre_ids'].cast<int>();
+    backdropPath     = json['backdrop_path'];
+    adult            = json['adult'];
+    overview         = json['overview'];
+    releaseDate      = json['release_date'];
+
+
+  }
+
+  getPosterImg(){
+
+    if(posterPath == null){
+      return 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png';
+    }else{
+      return 'https://image.tmdb.org/t/p/w500/$posterPath';
+    }
+
+  }
+
+ /*  getPopularImg(){
+    if(posterPath == null){
+      return 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png';
+    }else{
+      return 'https://image.tmdb.org/t/p/w500/$backdropPath';
+    }
+  } */
+
+}
