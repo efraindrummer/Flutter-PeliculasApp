2b0f68baa0f113c27478c3c2bad10da0a106483e
Efrain May <efrain_may_75@hotmail.com>
2021-01-09 17:13:30 -0600

    servicio para btener actores de la pelicula

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:peliculas/src/models/actores_model.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -65,5 +66,20 @@ class PeliculasProvider {
 
     _cargando = false;
     return resp;
+  }
+
+  Future<List<Actor>> getCast(String peliId) async{
+
+    final url = Uri.https(_url, '3/movie/$peliId/credits', {
+      'api_key'   : _apiKey,
+      'language'  : _language
+    });
+
+    final resp = await http.get(url);
+    final decodeData = json.decode(resp.body);
+
+    final cast = new Cast.fromJsonList(decodeData['cast']);
+
+    return cast.actores;
   }
 }
