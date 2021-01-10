40503d33d54c17d89503614186965e8ccbe4ea5a
Efrain May <efrain_may_75@hotmail.com>
2021-01-09 22:48:57 -0600

    buscador de peliculas y aplicacion terminada - lista para su primera produccion

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 
 class DataSearch extends SearchDelegate {
 
   String seleccion = '';
+  final peliculasProvider = new PeliculasProvider();
 
   final peliculas = [
     'Spiderman',
@@ -22,68 +25,86 @@ class DataSearch extends SearchDelegate {
   @override
   List<Widget> buildActions(BuildContext context) {
       //las acciones del appbar
-      return [
-        IconButton(
-          icon: Icon(Icons.clear),
-          onPressed: (){
-            query = '';
-          },
-        )
-      ];
-    }
-  
-    @override
-    Widget buildLeading(BuildContext context) {
-      // icono a la zquierda del appBar
-      return IconButton(
-        icon: AnimatedIcon(
-          icon: AnimatedIcons.menu_arrow,
-          progress: transitionAnimation,
-        ),
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
         onPressed: (){
-          close(context, null);
+          query = '';
         },
-      );
-    }
-  
-    @override
-    Widget buildResults(BuildContext context) {
-      // es el builder o la instrccion que crea lo resultados que vamos a mostrar
-      return Center(
-        child: Container(
-          height: 100.0,
-          width: 100.0,
-          color: Colors.redAccent,
-          child: Text(seleccion),
-        ),
-      );
-    }
-  
-    @override
-    Widget buildSuggestions(BuildContext context) {
-    // son las sugerencias cuando la persona escribe
+      )
+    ];
+  }
 
-    //retornar la lista de peliculas que una persona escribe en query
-    final listaSugerida = (query.isEmpty) ? peliculasRecientes : peliculas.where(
-      (p) => p.toLowerCase().startsWith(query.toLowerCase())
-    ).toList();
-
-    return ListView.builder(
-      itemCount: listaSugerida.length,
-      itemBuilder: (context, i){
-        return ListTile(
-          leading: Icon(Icons.movie),
-          title: Text(listaSugerida[i]),
-          onTap: (){
-            seleccion = listaSugerida[i];
-            //se nesecita el metodo para construir los resultados
-            showResults(context);
-          },
-        );
+  @override
+  Widget buildLeading(BuildContext context) {
+    // icono a la zquierda del appBar
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: (){
+        close(context, null);
       },
     );
   }
 
+  @override
+  Widget buildResults(BuildContext context) {
+    // es el builder o la instrccion que crea lo resultados que vamos a mostrar
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.redAccent,
+        child: Text(seleccion),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // son las sugerencias cuando la persona escribe
+    
+    if(query.isEmpty){
+      return Container();
+    }
+
+    return FutureBuilder(
+      future: peliculasProvider.buscarPelicula(query),
+      builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+        
+        if(snapshot.hasData){
+
+          final peliculas = snapshot.data;
+
+          return ListView(
+            children: peliculas.map((pelicula) {
+              return ListTile(
+                leading: FadeInImage(
+                  image: NetworkImage(pelicula.getPosterImg()),
+                  placeholder: AssetImage('assets/img/no-image.jpg'),
+                  width: 50.0,
+                  fit: BoxFit.contain,
+                ),
+                title: Text(pelicula.title),
+                subtitle: Text(pelicula.originalTitle),
+                onTap: (){
+                  close(context, null);
+                  pelicula.uniqueId = '';
+                  Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+                },
+              );
+            }).toList()
+          );
+        }else{
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
 
 
 }
